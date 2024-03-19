bb822cad73f1ba6008e62a6fad9b262c5ba5c2d8
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-03-19 12:33:54 -0400

    updated ts

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const users = require('./controllers/users');
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => (
-    res.send('Hello World!')
-));
+app.get('/', (req, res) => {
+  res.send('Hello!!!!!!!')
+})
+.use('/users', users)
 
-app.listen(PORT, () => (
-    console.log('App listening at http://localhost:$(PORT)')
-));
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`)
+});
